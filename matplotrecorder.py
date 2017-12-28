492b4ba8aabf82a54da9175e49462532b8f01011
Kai <kaikai_j2013@163.com>
2017-12-27 22:40:32 -0500

    Add into vscode ws; change variable name; if do nothing, don't print.

--- matplotrecorder.py
+++ matplotrecorder.py
@@ -22,11 +22,11 @@
 """
 
 import os
-import matplotlib.pyplot as plt
 import subprocess
+import matplotlib.pyplot as plt
 
-iframe = 0
-donothing = False  # switch to stop all recordering
+FRAME_CNT = 0
+DO_NOTHING = True  # switch to stop all recordering
 
 
 def save_frame():
@@ -34,10 +34,10 @@ def save_frame():
     Save a frame for movie
     """
 
-    if not donothing:
-        global iframe
-        plt.savefig("recorder" + '{0:04d}'.format(iframe) + '.png')
-        iframe += 1
+    if not DO_NOTHING:
+        global FRAME_CNT
+        plt.savefig("recorder" + '{0:04d}'.format(FRAME_CNT) + '.png')
+        FRAME_CNT += 1
 
 
 def save_movie(fname, d_pause):
@@ -45,36 +45,37 @@ def save_movie(fname, d_pause):
     Save movie as gif or video (.mp4 recommended)
     """
 
-    filename, extension = os.path.splitext(fname)
-    
-    print("Saving movie......")
+    if not DO_NOTHING:
+        _, extension = os.path.splitext(fname)
 
-    if not donothing:
+        print("Saving movie......")
+
         if extension == ".gif":
             cmd = "convert -delay " + str(int(d_pause * 100)) + " recorder*.png " + fname
         else:
             ## imagemagick (convert) cannot find ffmpeg
-            cmd = "ffmpeg -framerate " + str(int(d_pause * 100)) + " -pattern_type glob -i 'recorder*.png' -c:v libx264 " + fname
+            cmd = "ffmpeg -framerate " + str(int(d_pause * 100)) + \
+                  " -pattern_type glob -i 'recorder*.png' -c:v libx264 " + fname
 
         subprocess.call(cmd, shell=True)
         cmd = "rm recorder*.png"
         subprocess.call(cmd, shell=True)
 
-    print("Movie has been saved! (Maybe a bad movie, please check it!)")
+        print("Movie has been saved! (Maybe a bad movie, please check it!)")
 
 
 if __name__ == '__main__':
     print("A sample recording start")
     import math
 
-    time = range(50)
+    TIME = range(50)
 
-    x1 = [math.cos(t / 10.0) for t in time]
-    y1 = [math.sin(t / 10.0) for t in time]
-    x2 = [math.cos(t / 10.0) + 2 for t in time]
-    y2 = [math.sin(t / 10.0) + 2 for t in time]
+    X1 = [math.cos(t / 10.0) for t in TIME]
+    Y1 = [math.sin(t / 10.0) for t in TIME]
+    X2 = [math.cos(t / 10.0) + 2 for t in TIME]
+    Y2 = [math.sin(t / 10.0) + 2 for t in TIME]
 
-    for ix1, iy1, ix2, iy2 in zip(x1, y1, x2, y2):
+    for ix1, iy1, ix2, iy2 in zip(X1, Y1, X2, Y2):
         plt.plot(ix1, iy1, "xr")
         plt.plot(ix2, iy2, "xb")
         plt.axis("equal")
@@ -82,5 +83,5 @@ if __name__ == '__main__':
 
         save_frame()  # save each frame
 
-    # save_movie("test_animation.gif", 0.1)
-    save_movie("test_animation.mpg", 0.1)
+    save_movie("test_animation.gif", 0.1)
+    # save_movie("test_animation.mp4", 0.1)
